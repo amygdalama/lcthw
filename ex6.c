b5db9f959c363cbb469ad9b1a181241be40e51f1
Amy Hanlon <amyehanlon@gmail.com>
2014-07-12 14:49:47 -0400

    fix segfault!

--- ex6.c
+++ ex6.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
     printf("You are %d miles away.\n", distance);
     printf("You have %f levels of power.\n", power);
     printf("You have %f awesome super powers.\n", super_power);
-    printf("My name is %s %c. %s\n", initial, first_name, last_name);
+    printf("My name is %s %c. %s\n", first_name, initial, last_name);
 
     return 0;
 }

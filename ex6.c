2937646acf1fe604679a7da6efc3d51a727082bd
Amy Hanlon <amyehanlon@gmail.com>
2014-07-12 14:48:02 -0400

    ex 6

---
+++ ex6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int distance = 100;
+    float power = 2.345f;
+    double super_power = 56789.4532;
+    char initial = 'E';
+    char first_name[] = "Amy";
+    char last_name[] = "Hanlon";
+
+    printf("You are %d miles away.\n", distance);
+    printf("You have %f levels of power.\n", power);
+    printf("You have %f awesome super powers.\n", super_power);
+    printf("My name is %s %c. %s\n", first_name, initial, last_name);
+
+    return 0;
+}
